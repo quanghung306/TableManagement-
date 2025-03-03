d5cd8a383e881091c085ea9f94944fda09e3205a
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2025-03-03 14:33:22 +0700

    Update: fix delete columnUserStore

--- src/components/users/EditUser.vue
+++ src/components/users/EditUser.vue
@@ -1,38 +1,40 @@
 <template>
   <div
     v-if="isOpen"
-    class="fixed inset-0 flex items-center justify-center z-50">
+    class="fixed inset-0 flex items-center justify-center z-50"
+  >
     <div class="absolute z-0 inset-0 flex bg-gray-900 opacity-50"></div>
     <div class="bg-white p-6 rounded shadow-lg w-96 absolute z-10">
-      <h2 class="text-xl font-semibold mb-4">{{ modelValue.id ? "Edit User" : "Add User" }}</h2>
+      <h2 class="text-xl font-semibold mb-4">
+        {{ modelValue.id ? "Edit User" : "Add User" }}
+      </h2>
       <form @submit.prevent="handleSubmit">
         <div v-for="column in columns" :key="column.key" class="mb-4">
-            <label class="block text-gray-700 capitalize">{{ column.key }}</label>
-            <select
-              v-if="column.key === 'Gender'"
-              v-model="editableUser[column.key]"
-              class="border p-2 rounded w-full mt-1"
-            >
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-            <select
-              v-else-if="column.key === 'Status'"
-              v-model="editableUser[column.key]"
-              class="border p-2 rounded w-full mt-1"
-              
-            >
-              <option value="Active">Active</option>
-              <option value="Inactive">Inactive</option>
-              <option value="Pending">Pending</option>
-            </select>
-            <component
-              v-else
-              :is="column.inputType || 'input'"
-              v-model="editableUser[column.key]"
-              class="border p-2 rounded w-full mt-1"
-              :placeholder="'Enter ' + column.key"
-            />
+          <label class="block text-gray-700 capitalize">{{ column.key }}</label>
+          <select
+            v-if="column.key === 'Gender'"
+            v-model="editableUser[column.key]"
+            class="border p-2 rounded w-full mt-1"
+          >
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          <select
+            v-else-if="column.key === 'Status'"
+            v-model="editableUser[column.key]"
+            class="border p-2 rounded w-full mt-1"
+          >
+            <option value="Active">Active</option>
+            <option value="Inactive">Inactive</option>
+            <option value="Pending">Pending</option>
+          </select>
+          <component
+            v-else
+            :is="column.inputType || 'input'"
+            v-model="editableUser[column.key]"
+            class="border p-2 rounded w-full mt-1"
+            :placeholder="'Enter ' + column.key"
+          />
         </div>
         <div class="flex justify-end">
           <button
@@ -55,25 +57,30 @@
 </template>
 <script setup>
 import { ref, watch } from "vue";
-import { useColumnStore } from "../../stores/columnUserStore";
+
 import { useUserStore } from "../../stores/userStore";
 const props = defineProps({
   modelValue: {
     type: Object,
-    default: () => ({})
+    default: () => ({}),
   },
   isOpen: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 const emit = defineEmits(["save", "close"]);
 
-const columnStore = useColumnStore();
 const userStore = useUserStore();
 
-const columns = columnStore.columns;
 const editableUser = ref({});
+const columns = ref([
+  { key: "Name" },
+  { key: "Position" },
+  { key: "Status" },
+  { key: "Gender" },
+  { key: "Email" },
+]);
 
 // Cập nhật editableUser khi modelValue thay đổi
 watch(
@@ -88,7 +95,7 @@ async function handleSubmit() {
   const result = await userStore.saveUser({ ...editableUser.value }); // Chờ kết quả
   console.log("🚀 ~ handleSubmit ~ result:", result);
   if (result !== false) {
-    close(); 
+    close();
   }
 }
 
